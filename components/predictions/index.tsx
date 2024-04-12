161f04bdf028e10b8963bca006fa9797bcf78068
Marcos Velasco <devslife7@gmail.com>
2024-04-12 00:11:43 -0400

    added the logic for predictions table

--- components/predictions/index.tsx
+++ components/predictions/index.tsx
@@ -2,31 +2,79 @@ import Image from "next/image"
 import background from "../../public/images/predictions-background.png"
 import userPredictions from "../../data/predictions.json"
 import Link from "next/link"
+import fixtures from "../../data/fixtures.json"
 
 export default function Predictions() {
+  // Sort by match start time
+  fixtures.response.sort((a, b) => {
+    return a.fixture.timestamp - b.fixture.timestamp
+  })
+
+  // API parsed array of official match results
+  const getParsedResultsArray = () => {
+    let officialResults: (number | string)[] = []
+    fixtures.response.forEach(fixture => {
+      const gameStatus = fixture.fixture.status.short
+      const goalsHome = fixture.goals.home
+      const goalsAway = fixture.goals.away
+      const homeTeamId = fixture.teams.home.id
+      const awayTeamId = fixture.teams.away.id
+
+      if (gameStatus === "FT") {
+        if (goalsHome === goalsAway) officialResults.push("TIE")
+        else if (goalsHome > goalsAway) officialResults.push(homeTeamId)
+        else officialResults.push(awayTeamId)
+      }
+    })
+    return officialResults
+  }
+
+  // Compare official results with user predictions and return how many correct predictions
+  const findCorrectPredictions = (parsedResultsArray: (number | string)[], predictions: (number | string)[]) => {
+    if (parsedResultsArray.length === 0) return
+    let correctPredictions = 0
+    for (let i = 0; i < parsedResultsArray.length; i++) {
+      parsedResultsArray[i] === predictions[i] && correctPredictions++
+    }
+    return correctPredictions
+  }
+
+  // Add user ranking to array
+  const addUserRanking = (users: any) => {
+    let userRanking = 1
+    let userArray: any = []
+    for (let i = 0; i < users.length; i++) {
+      if (i !== 0) users[i].correctPredictions !== users[i - 1].correctPredictions && userRanking++
+      userArray = [...userArray, { ...users[i], userRanking: userRanking }]
+    }
+    return userArray
+  }
+
   const renderUserPredictions = () => {
-    // get official results
-    // const officialResults = getOfficialResult()
+    // Get user points, compare with user preidctions with api parsed array of official match results.
+    const userWithCorrectPredictions = userPredictions.map(user => {
+      let correctPredictions = findCorrectPredictions(getParsedResultsArray(), user.predictionGroupStage)
+      return { ...user, correctPredictions }
+    })
+    console.log("userRightMatches", userWithCorrectPredictions)
 
-    // Add user right matches
-    // const userRightMatches = userData.map(user => {
-    //   let rightMatches = findCorrectPredictions(officialResults, user.predictionsGS)
-    //   return { ...user, rightMatches: rightMatches }
-    // })
+    // Sort users alphabetically && Sort by correct predictions
+    userWithCorrectPredictions.sort((a, b) => a.name.localeCompare(b.name)) // Sort by name
+    userWithCorrectPredictions.sort((a, b) => (b.correctPredictions || 0) - (a.correctPredictions || 0)) // Sort by prediction
+    const userArray = addUserRanking(userWithCorrectPredictions)
 
-    // Sort users alphabetically || Sort by correct predictions || Add right matches
-    // userRightMatches.sort((a, b) => a.first_name.localeCompare(b.first_name))
-    // userRightMatches.sort((a, b) => b.rightMatches - a.rightMatches)
-    // const userArray = addUserRanking(userRightMatches)
-    return userPredictions.map((user, idx) => (
+    return userArray.map((user: any, idx: number) => (
       <div key={idx} className="flex justify-between text-lg font-bold">
-        <Link href={`/${user.id}`}>
-          {idx + 1}
-          st {user.name}
-        </Link>
+        <div className="flex">
+          <div className="w-12">
+            {user.userRanking}
+            {user.userRanking === 1 ? "st" : user.userRanking === 2 ? "nd" : user.userRanking === 3 ? "rd" : "th"}
+          </div>
+          <Link href={`/${user.id}`}>{user.name}</Link>
+        </div>
         <div className="flex space-x-10">
           <p className="text-xl">
-            22 <span className="text-sm">pts</span>
+            {user.correctPredictions} <span className="text-sm">pts</span>
           </p>
         </div>
       </div>
