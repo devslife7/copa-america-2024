f19c27c4a2af60e07e0a42bb9c91b4fd49e3db9c
Marcos Velasco <devslife7@gmail.com>
2024-04-18 01:35:05 -0400

    added svg folder

--- app/[id]/Fixtures.tsx
+++ app/[id]/Fixtures.tsx
@@ -1,34 +1,66 @@
 import AllFixtures from "@/data/fixtures.json"
+import CheckMarkSVG from "@/public/svgs/check-mark"
 import Image from "next/image"
 
 export default function Fixtures({ predictionGroupStage }: { predictionGroupStage: any }) {
   console.log("predictionGroupStage", predictionGroupStage)
 
   const renderFixtures = () => {
-    return AllFixtures.response.map((fixture: any) => {
-      return (
-        <div key={fixture.fixture.id} className="flex justify-between items-center bg-secondary p-4 rounded-md my-2 text-sm">
-          <div className="flex items-center">
-            {renderFlag(fixture.teams.home)}
-            <span className="ml-2">{fixture.teams.home.name}</span>
-          </div>
-          <div className="flex items-center">
-            <span>{fixture.goals.home}</span>
-            <span> - </span>
-            <span>{fixture.goals.away}</span>
-          </div>
-          <div className="flex items-center">
-            <span className="mr-2">{fixture.teams.away.name}</span>
-            {renderFlag(fixture.teams.away)}
-          </div>
+    const fixture = AllFixtures.response[0]
+
+    return (
+      <div key={fixture.fixture.id} className="flex bg-secondary p-4 my-2 text-sm">
+        <div className="w-10 text-2xl text-gray-400 text-center">
+          <CheckMarkSVG className="text-green-400 mx-auto mb-1" />
+          <div className="text-xs">{"|"}</div>
+          <div className="text-xs">{"|"}</div>
+          <div className="text-xs">{"|"}</div>
+          <div className="text-xs">{"|"}</div>
         </div>
-      )
-    })
+        <div className="w-full">
+          <div className="flex justify-between">
+            <h2 className="mb-2">Argentina</h2>
+            <span>+1</span>
+          </div>
+
+          {renderFixture(fixture)}
+        </div>
+      </div>
+    )
+
+    // return AllFixtures.response.map((fixture: any) => {
+    //   return (
+    //     <div key={fixture.fixture.id} className="flex justify-between items-center bg-secondary p-4 rounded-md my-2 text-sm">
+    //       <div className="flex items-center">
+    //         {renderFlag(fixture.teams.home)}
+    //         <span className="ml-2">{fixture.teams.home.name}</span>
+    //       </div>
+    //       <div className="flex items-center">
+    //         <span>{fixture.goals.home}</span>
+    //         <span> - </span>
+    //         <span>{fixture.goals.away}</span>
+    //       </div>
+    //       <div className="flex items-center">
+    //         <span className="mr-2">{fixture.teams.away.name}</span>
+    //         {renderFlag(fixture.teams.away)}
+    //       </div>
+    //     </div>
+    //   )
+    // })
   }
   return (
     <div>
-      <div>Fixtures</div>
+      <div className="text-3xl m-5 ">Fixtures</div>
       {renderFixtures()}
+    </div>
+  )
+}
+
+const renderFixture = (fixture: any) => {
+  return (
+    <div className="p-2 bg-blue-200 rounded-md w-full">
+      <h2>{fixture.teams.home.name}</h2>
+      <h2>{fixture.teams.away.name}</h2>
     </div>
   )
 }
