728941b8121e45031512d2205e69e67fe8418d6c
Marcos Velasco <devslife7@gmail.com>
2024-04-19 01:53:30 -0400

    Add Unix Timestamp parser

--- app/[id]/Fixtures.tsx
+++ app/[id]/Fixtures.tsx
@@ -6,13 +6,16 @@ import TeamIds from "@/data/team-ids.json"
 import { useSearchParams } from "next/navigation"
 import XMarkSVG from "@/public/svgs/x-mark"
 import CircleSVG from "@/public/svgs/circle"
+import { format, fromUnixTime } from "date-fns"
 
 export default function Fixtures({ predictionGroupStage }: { predictionGroupStage: any }) {
   const searchParams = useSearchParams()
   const correctPredictionsArray = searchParams.get("correctPredictionsArray")?.split("") // Gets the string of url and splits it into an array
 
+  console.log("predictionGroupStage", predictionGroupStage)
+
   const renderFixtures = () => {
-    const idx = 0
+    const idx = 1
     const fixture = AllFixtures.response[idx]
 
     const teamId = predictionGroupStage[idx]
@@ -79,10 +82,27 @@ const renderFixture = (fixture: any) => {
   return (
     <div className="flex justify-between p-2 bg-gray-800 rounded-md w-full">
       <div>
-        <h2>{fixture.teams.home.name}</h2>
+        <h2 className="mb-1">{fixture.teams.home.name}</h2>
         <h2>{fixture.teams.away.name}</h2>
       </div>
-      {/* <div>{fixture.fixture.</div> */}
+      {fixture.fixture.status.short === "FT" ? (
+        <div className="flex">
+          <div className="border-r-2 border-gray-600 pr-4">
+            <h2 className="mb-1">{fixture.goals.home}</h2>
+            <h2>{fixture.goals.away}</h2>
+          </div>
+          <div className="flex items-center text-sm">
+            <div className="px-4">
+              <div>Final</div>
+              <span>Score</span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-end text-gray-300 text-sm">
+          {String(format(fromUnixTime(fixture.fixture.timestamp), "MMMM dd, h aaa"))}
+        </div>
+      )}
     </div>
   )
 }
