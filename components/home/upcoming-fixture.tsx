30af81308919fdf437303b76e7efa5749aec4fc5
Marcos Velasco <devslife7@gmail.com>
2024-04-22 21:27:59 -0400

    added stage to upcoming fixtures

--- components/home/upcoming-fixture.tsx
+++ components/home/upcoming-fixture.tsx
@@ -3,14 +3,20 @@ import RenderFixture from "../shared/render-fixture"
 import fixtures from "../../data/fixtures.json"
 
 export default function UpcomingFixtures() {
+  // Calculate upcioming and previous fixtures
+  // in this format: { upcoming: [fixture1, fixture2], previous: [fixture3] }
+  const upcoming = { upcoming: [26, 25], previous: [0] }
   return (
     <section className="px-6 pt-[100px] pb-[90px] text-white">
       <div className="max-w-[600px] mx-auto">
         <h2 className="text-3xl font-bold z-50 mb-4">Upcoming</h2>
-        <RenderFixture fixture={fixtures.response[26]} className="mt-2 mb-4" />
-        <RenderFixture fixture={fixtures.response[25]} className="mt-2 mb-8" />
-        <div className="text-white font-bold text-xl">Previous</div>
-        <RenderFixture fixture={fixtures.response[0]} className="mt-2 mb-8" />
+        <h3>{fixtures.response[upcoming.upcoming[0]].league.round}</h3>
+        <RenderFixture fixture={fixtures.response[upcoming.upcoming[0]]} className="mt-2 mb-4" />
+        <h3>{fixtures.response[upcoming.upcoming[1]].league.round}</h3>
+        <RenderFixture fixture={fixtures.response[upcoming.upcoming[1]]} className="mt-2 mb-8" />
+        <div className="text-white font-bold text-2xl mb-2">Previous</div>
+        <h3>{fixtures.response[0].league.round}</h3>
+        <RenderFixture fixture={fixtures.response[upcoming.previous[0]]} className="mt-2 mb-8" />
       </div>
     </section>
   )
