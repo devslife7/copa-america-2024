d17a26dc39358cc1ca84c48798d67c6d32f0d235
Marcos Velasco <devslife7@gmail.com>
2024-04-22 21:18:15 -0400

    renamed upcoming component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Credits from "@/components/home/credits"
 import Hero from "@/components/home/hero"
-import UpcomingFixtures from "@/components/home/next-fixture"
 import Predictions from "@/components/home/predictions"
 import ThanksTo from "@/components/home/thanks-to"
+import UpcomingFixtures from "@/components/home/upcoming-fixture"
 
 export default function Home() {
   return (
