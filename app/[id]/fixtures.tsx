bd2e0ed8f001e94f627062adddcccabd0e485eec
Marcos Velasco <devslife7@gmail.com>
2024-04-30 16:48:51 -0400

    quartes and semis renders correctly for demo

--- app/[id]/fixtures.tsx
+++ app/[id]/fixtures.tsx
@@ -1,19 +1,16 @@
 "use client"
-import AllFixtures from "@/data/fixtures2022.json"
 import CheckMarkSVG from "@/public/svgs/check-mark"
-import Image from "next/image"
 import TeamIds from "@/data/team-ids.json"
 import { usePathname, useSearchParams } from "next/navigation"
 import XMarkSVG from "@/public/svgs/x-mark"
 import CircleSVG from "@/public/svgs/circle"
-import { format, fromUnixTime } from "date-fns"
 import { cn } from "@/lib/utils"
 import RenderFixture from "@/components/shared/render-fixture"
 import { useState } from "react"
 import { useFixturesContext } from "@/context/fixtures"
 
 export default function Fixtures() {
-  const [sortBy, setSortBy] = useState("Groups")
+  const [sortBy, setSortBy] = useState("Quarters")
   const { users, fixtures } = useFixturesContext()
   const pathname = usePathname()
   const id = pathname.split("/")[1]
@@ -98,70 +95,61 @@ export default function Fixtures() {
     })
   }
   const renderEliminationStage = (stage: string) => {
+    let teamsInQuarterFinals: any = []
     let stageFixtures = []
-    if (stage === "Quarter-finals") stageFixtures = fixtures.quarterFinalFixtures
-    else if (stage === "Semi-finals") stageFixtures = fixtures.semiFinalFixtures
-    else if (stage === "Final") stageFixtures = fixtures.finalFixtures
+    let userPredictions = []
+    if (stage === "Quarter-finals") {
+      stageFixtures = fixtures.quarterFinalFixtures
+      if (stageFixtures.length === 0) return null
+      userPredictions = user.predictions.quarterFinals
 
-    // if guess from user exist in the array of correct predictions, render checkmark, else render xmark if all matches are fnished, else render black circle
+      stageFixtures.map((fixture: any) => {
+        teamsInQuarterFinals?.push(fixture.teams.home.id)
+        teamsInQuarterFinals?.push(fixture.teams.away.id)
+      })
+    } else if (stage === "Semi-finals") {
+      stageFixtures = fixtures.semiFinalFixtures
+      userPredictions = user.predictions.semiFinals
+
+      stageFixtures.map((fixture: any) => {
+        teamsInQuarterFinals?.push(fixture.teams.home.id)
+        teamsInQuarterFinals?.push(fixture.teams.away.id)
+      })
+    } else if (stage === "Final") {
+      stageFixtures = fixtures.finalFixtures
+      userPredictions = user.predictions.final
+    }
+
+    console.log("teamsInQuarterFinals", teamsInQuarterFinals)
+    console.log("userPredictions", userPredictions)
 
     return (
       <>
-        {/* <div className="relative w-10 text-xl text-gray-400 text-center flex justify-center">
-          {fixture.fixture.status.short === "FT" || fixture.fixture.status.short === "PEN" ? (
-            (isCorrectPredictionArray ?? [])[idx] ? (
-              <CheckMarkSVG className="text-green-400" />
-            ) : (
-              <XMarkSVG className="text-red-400 text-3xl" />
-            )
-          ) : (
-            <CircleSVG className="text-gray-400" />
-          )}
-
-          <div className={cn("absolute left-4 top-[1.4rem]", { hidden: stageFixtures.length - 1 === idx })}>
-            <div className="text-xs">{"|"}</div>
-            <div className="text-xs">{"|"}</div>
-            <div className="text-xs">{"|"}</div>
-            <div className="text-xs">{"|"}</div>
-            <div className="text-xs">{"|"}</div>
-            <div className="text-xs">{"|"}</div>
-            <div className="text-xs">{"|"}</div>
-          </div>
-        </div> */}
-        {stageFixtures.map((fixture: any, idx: number) => {
-          return (
-            <div key={fixture.fixture.id} className="flex h-[140px] p-4 pr-6">
-              <div className="relative w-10 text-xl text-gray-400 text-center flex justify-center">
-                {fixture.fixture.status.short === "FT" || fixture.fixture.status.short === "PEN" ? (
-                  (isCorrectPredictionArray ?? [])[idx] ? (
+        <div className="pt-4 px-[22.5px] pb-6">
+          {userPredictions.map((team: string, idx: number) => {
+            return (
+              <div key={idx} className="flex">
+                <div className="text-center">
+                  {teamsInQuarterFinals.includes(+team) ? (
+                    <CheckMarkSVG className="text-green-400" />
+                  ) : false ? (
                     <XMarkSVG className="text-red-400" />
                   ) : (
-                    <CheckMarkSVG className="text-green-400 " />
-                  )
-                ) : (
-                  <CircleSVG className="text-gray-400" />
-                )}
-
-                <div className={cn("absolute left-4 top-[1.4rem]", { hidden: stageFixtures.length - 1 === idx })}>
-                  <div className="text-xs">{"|"}</div>
-                  <div className="text-xs">{"|"}</div>
-                  <div className="text-xs">{"|"}</div>
-                  <div className="text-xs">{"|"}</div>
-                  <div className="text-xs">{"|"}</div>
-                  <div className="text-xs">{"|"}</div>
-                  <div className="text-xs">{"|"}</div>
+                    <CircleSVG className="text-gray-400" />
+                  )}
+                  <div className={cn("text-xs", { hidden: userPredictions.length == idx + 1 })}>{"|"}</div>
+                </div>
+                <div className="ml-2">
+                  <h2>{TeamIds[team as keyof typeof TeamIds]}</h2>
                 </div>
               </div>
-              <div className="w-full">
-                <div className="flex justify-between">
-                  <h2 className="mb-2">{TeamIds[user.predictions.groupStage[idx] as keyof typeof TeamIds]}</h2>
-                  {/* {fixture.fixture.status.short === "FT" &&
-                    ((isCorrectPredictionArray ?? [])[idx] ? <span>+1</span> : <span>+0</span>)} */}
-                  <div className="text-sm">{fixture.league.round}</div>
-                </div>
-
-                <RenderFixture fixture={fixture} />
-              </div>
+            )
+          })}
+        </div>
+        {stageFixtures.map((fixture: any, idx: number) => {
+          return (
+            <div key={fixture.fixture.id} className="flex px-6 py-3">
+              <RenderFixture fixture={fixture} />
             </div>
           )
         })}
