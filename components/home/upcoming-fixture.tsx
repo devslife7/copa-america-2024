b7d9e1f0d1b6c61512805c3dc7b49a6fe79733e6
Marcos Velasco <devslife7@gmail.com>
2024-07-07 00:04:33 -0400

    updated upcoming matches

--- components/home/upcoming-fixture.tsx
+++ components/home/upcoming-fixture.tsx
@@ -10,23 +10,29 @@ export default function UpcomingFixtures() {
   const upcoming = { upcoming: [26, 25], previous: [0] }
 
   const { fixtures } = useFixturesContext()
-  if (fixtures.upcomingFixtures.upcoming.length == 0) return null
-  if (fixtures.upcomingFixtures.upcoming[0].league === undefined) return null
 
-  const upcomingFixtures = fixtures.upcomingFixtures.upcoming
+  const upcomingFixtures = fixtures.upcomingFixtures.upcoming.length > 0 ? fixtures.upcomingFixtures.upcoming : []
   const pastFixture = fixtures.upcomingFixtures.past
 
   return (
     <section className="px-6 pt-[100px] pb-[90px] text-white">
       <div className="max-w-[600px] mx-auto">
-        <h2 className="text-3xl font-bold z-50 mb-4">Upcoming</h2>
-        <h3>{upcomingFixtures[0].league.round}</h3>
-        <RenderFixture fixture={upcomingFixtures[0]} className="mt-2 mb-4" />
-        <h3>{upcomingFixtures[1].league.round}</h3>
-        <RenderFixture fixture={upcomingFixtures[1]} className="mt-2 mb-8" />
-        <div className="text-white font-bold text-2xl mb-2">Previous</div>
-        <h3>{pastFixture.league.round}</h3>
-        <RenderFixture fixture={pastFixture} className="mt-2 mb-8" />
+        {upcomingFixtures.length > 0 && (
+          <div>
+            <h2 className="text-3xl font-bold z-50 mb-4">Upcoming</h2>
+            <h3>{upcomingFixtures[0].league.round}</h3>
+            <RenderFixture fixture={upcomingFixtures[0]} className="mt-2 mb-4" />
+            {/* <h3>{upcomingFixtures[1].league.round}</h3>
+            <RenderFixture fixture={upcomingFixtures[1]} className="mt-2 mb-8" /> */}
+          </div>
+        )}
+        {pastFixture.fixture.id && (
+          <div>
+            <div className="text-white font-bold text-2xl mb-2">Previous</div>
+            <h3>{pastFixture.league?.round}</h3>
+            <RenderFixture fixture={pastFixture} className="mt-2 mb-8" />
+          </div>
+        )}
       </div>
     </section>
   )
